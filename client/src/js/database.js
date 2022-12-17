bd78027dc42d4eb6b4fed4b2cb74e464b0c317a4
Catherine <cpolknytx@gmail.com>
2022-12-17 11:53:35 -0600

    added comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   // console.log('putDb not implemented');
   const jateDB = await openDB('jate', 1);
@@ -23,7 +23,7 @@ export const putDb = async (content) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// logic for a method that gets all the content from the database
 export const getDb = async () => {
   // console.error('getDb not implemented');
   const jateDB = await openDB('jate', 1);
